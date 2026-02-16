34d02aace382b5d7a96895f03208c7c0e27ea95f
Naderadelp <naderadelpp@gmail.com>
2026-02-16 17:48:19 +0200

    use api instead of web

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NotificationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/send', [NotificationController::class, 'send']);
